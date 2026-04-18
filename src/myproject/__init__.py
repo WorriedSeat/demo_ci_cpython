196544111f4acf4c9241e09fb28d58ff1e56e7e1
WorriedSeat <vanish_for.white@mail.ru>
2026-04-18 16:03:02 +0300

    initial project structure

---
+++ src/myproject/__init__.py
@@ -0,0 +1,4 @@
+from .example import add, multiply
+
+__version__ = "0.1.0"
+__all__ = ["add", "multiply"]
---
+++ src/myproject/example.py
@@ -0,0 +1,8 @@
+def add(a: int, b: int) -> int:
+    """Sums two numbers."""
+    return a + b
+
+
+def multiply(a: int, b: int) -> int:
+    """Multiplies two numbers."""
+    return a * b
